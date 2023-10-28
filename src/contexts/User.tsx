b53ede2390acad8e2ccc7718ab0b0c08acdceb6f
athensclub <athens_club@hotmail.com>
2023-10-28 16:58:41 +0700

    refactor: move user to contexts folder

--- src/contexts/User.tsx
+++ src/contexts/User.tsx
@@ -1,6 +1,7 @@
 "use client";
 import usePrevious from "@/app/hooks/usePrevious";
 import { createContext, useEffect, useState } from "react";
+import { finished } from "stream";
 
 export type UserContextData = {
   token: string | null;
@@ -13,15 +14,20 @@ export const UserContext = createContext<UserContextData>({
 
 export function UserContextProvider({
   children,
+  finishedInitialize,
 }: {
   children: React.ReactNode;
+    finishedInitialize: () => void;
 }) {
+  // const { finishedInitialize } = props;
   const [token, setToken] = useState<string | null>(null);
   const oldToken = usePrevious(token);
 
   // load from local storage on mount
   useEffect(() => {
     setToken(localStorage.getItem("cugetloveJWT"));
+    finishedInitialize();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
